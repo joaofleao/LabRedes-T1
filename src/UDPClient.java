635a0cd9c2c2cde55fca41590cdf9df254cae485
joaofleao <joaofleao@gmail.com>
2020-05-21 04:34:03 -0300

    commit inicial

---
+++ src/UDPClient.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class UDPClient {
+   public static void main(String args[]) throws Exception {
+      //declaracao do socket cliente
+      DatagramSocket clientSocket = new DatagramSocket();
+
+      System.out.println("Olá, para começar pressione 'Enter'");
+      
+      Scanner keyboard = new Scanner(System.in);
+      keyboard.nextLine();
+
+      sendData("iniciar", clientSocket, InetAddress.getByName("localhost"));
+
+      while (true) {
+         String question = receiveData(clientSocket);
+         if(question.equals("")) {
+            System.out.println("As perguntas acabaram!, Obrigado por jogar!");
+            break;
+         }
+         System.out.println(question);
+         
+         System.out.println("Digite a letra da alternativa correta");
+         String toSend = keyboard.nextLine();
+         toSend = toSend.toLowerCase();
+         switch(toSend) {
+            case "a": toSend = "0"; break;
+            case "b": toSend = "1"; break;
+            case "c": toSend = "2"; break;
+         }
+         sendData(toSend, clientSocket, InetAddress.getByName("localhost"));
+
+         System.out.println(receiveData(clientSocket));
+
+         keyboard.nextLine();
+         sendData("continuar", clientSocket, InetAddress.getByName("localhost"));
+      }
+
+   }
+   public static String format(String text) throws IOException {
+      Scanner scanner = new Scanner(text);
+      int code = Integer.parseInt(scanner.nextLine());
+      
+      if (code==-1) return "";
+
+      String formatted = "";
+      for (int i = code; i>0; i-- ) {
+         formatted = formatted + "\n" + scanner.nextLine();
+      }
+      
+      return formatted;
+
+   }
+   
+   private static String receiveData(DatagramSocket clientSocket) throws Exception{
+      //criar array de 1024
+      byte[] dataComing = new byte[1024];
+
+      // declara o pacote a ser recebido
+      DatagramPacket receivePacket = new DatagramPacket(dataComing, dataComing.length);
+
+      // recebe o pacote do servidor
+      clientSocket.receive(receivePacket);
+
+      //retorna o texto recebido string com texto
+      return format(new String (receivePacket.getData()));
+
+   }
+   
+   
+   private static boolean sendData(String text, DatagramSocket clientSocket, InetAddress IPAddress) throws Exception {
+         //criar array de 1024
+         byte[] dataGoing = text.getBytes();
+
+         // cria pacote com o dado, o endereço do server e porta do servidor
+         DatagramPacket sendPacket = new DatagramPacket(dataGoing, dataGoing.length, IPAddress, 3333);
+         
+         clientSocket.send(sendPacket);
+
+         return true;
+   }
+}
---
+++ src/UDPServer.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class UDPServer {
+      public static DatagramSocket serverSocket;
+      public static DatagramPacket receivePacket;
+      public static void main(String args[])  throws IOException {
+
+            // cria socket do servidor com a porta 1971
+            serverSocket = new DatagramSocket(3333);
+
+
+            Game jogo = new Game(true);
+            jogo.questions();
+            receivePacket();
+            
+            while(true) { 
+                  Question a = jogo.getRandomQuestion();
+                  if (a==null) {
+                        sendPacket("-1\n");
+                        System.out.println("Game Over");
+                        break;
+                  }
+                  
+                  sendPacket("4\n" +a.toString());
+
+                  int resposta = Character.getNumericValue(receivePacket().charAt(0));
+                  
+                  boolean resultado = jogo.answerQuestion(a, resposta);
+                  
+                  if (resultado) {
+                        sendPacket("4\nParabéns! Você acertou \nSua pontuação: " + jogo.getPlayer1Points() + "\nPerguntas certas seguidas: " + jogo.getPlayer1Fire() + "\n" + "Clique enter para continuar\n");
+                        
+                        receivePacket();
+                  }
+                  else {
+                        sendPacket("4\nQue pena! Você errou \nSua pontuação: " + jogo.getPlayer1Points() + "\nPerguntas certas seguidas: " + jogo.getPlayer1Fire() + "\n" + "Clique enter para continuar\n");
+                        receivePacket();
+                  }
+            }        
+      }
+
+      public static String receivePacket() throws IOException {
+            //cria array que vai ser recebido
+            byte[] dataComing = new byte[1024];
+
+            // declara o pacote a ser recebido
+            receivePacket = new DatagramPacket(dataComing, dataComing.length);
+
+            // recebe o pacote do cliente
+            serverSocket.receive(receivePacket);
+            
+            //retorna o texto que foi recebido
+            return (new String (receivePacket.getData()));
+      }
+
+
+      public static void sendPacket(String text) throws IOException {
+            
+            //converte texto para array de bytes
+            byte[] dataGoing = text.getBytes();
+
+            //pega o endereco que acabou de receber 
+            InetAddress IPAddress = receivePacket.getAddress();
+
+            //pega a porta que acabou de receber 
+            int port = receivePacket.getPort();
+
+            // cria pacote com o dado, o endereço do server e porta do servidor
+            DatagramPacket sendPacket = new DatagramPacket(dataGoing, dataGoing.length, IPAddress, port);
+
+            //envia o pacote
+            serverSocket.send(sendPacket);
+      }
+}
